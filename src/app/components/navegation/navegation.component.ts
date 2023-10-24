9190309dab0692a692aadcc7214a46ed0347f5e6
Felipe Torres Cresto <felipe.t.cresto@gmail.com>
2023-10-24 16:28:19 -0300

    feat: create header

--- src/app/components/navegation/navegation.component.ts
+++ src/app/components/navegation/navegation.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navegation',
@@ -9,5 +10,33 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./navegation.component.scss']
 })
 export class NavegationComponent {
+
+  public logoPath!: SafeResourceUrl;
+
+  
+  public menuItems = [
+    { label: 'Home', active: true },
+    { label: 'About Me', active: false },
+    { label: 'Resume', active: false },
+    { label: 'Skills', active: false },
+    { label: 'Projects', active: false },
+  ];
+  
+  constructor(private sanitizer: DomSanitizer) { }
+  
+  public ngOnInit() {
+    this.loadLogo();
+  }
+
+  private loadLogo() {
+    this.logoPath = this.sanitizer.bypassSecurityTrustResourceUrl('./assets/images/logo/logoFT.png');
+  }
+
+
+  public setActiveItem(item: any): void {
+    this.menuItems.forEach((menuItem) => {
+      menuItem.active = menuItem.label === item.label;
+    });
+  }
 
 }
