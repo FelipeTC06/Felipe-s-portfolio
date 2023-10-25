b520b5ab0fa693abaa75bdaaa61c01c2f6859db5
Felipe Torres Cresto <felipe.t.cresto@gmail.com>
2023-10-25 16:29:24 -0300

    feat: implement about me content

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,6 +1,7 @@
-import { Component, Renderer2 } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+import { Popover } from 'bootstrap'
 
 @Component({
   selector: 'app-about-me',
@@ -11,14 +12,29 @@ import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 })
 export class AboutMeComponent {
   public myImage!: SafeResourceUrl;
+  public goLinkedin: string = 'https://www.linkedin.com/in/felipe-torres-cresto/';
+  public goGitHub: string = 'https://github.com/FelipeTC06';
+  public downLoadCv: string = './assets/curriculo/CV Felipe Torres En.pdf';
+  public readMore: boolean = false;
 
   constructor(private sanitizer: DomSanitizer, private renderer: Renderer2) { }
 
   public ngOnInit() {
     this.loadImage();
+    this.popoOverOnInit();
+  }
+
+  private popoOverOnInit() {
+    Array.from(document.querySelectorAll('button[data-bs-toggle="popover"]'))
+      .forEach(popoverNode => new Popover(popoverNode))
   }
 
   private loadImage() {
     this.myImage = this.sanitizer.bypassSecurityTrustResourceUrl('./assets/images/image-perfil.png');
   }
+
+  public goReadMore() {
+    this.readMore = !this.readMore;
+  }
+
 }
--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from "../footer/footer.component";
 import { NavegationComponent } from "../navegation/navegation.component";
 import { HeadlineComponent } from "../headline/headline.component";
+import { AboutMeComponent } from "../about-me/about-me.component";
 
 @Component({
     selector: 'app-layout',
     standalone: true,
     templateUrl: './layout.component.html',
     styleUrls: ['./layout.component.scss'],
-    imports: [CommonModule, FooterComponent, NavegationComponent, HeadlineComponent]
+    imports: [CommonModule, FooterComponent, NavegationComponent, HeadlineComponent, AboutMeComponent]
 })
 export class LayoutComponent {
 
